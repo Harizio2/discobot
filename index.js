6279bd0d6bc88481f8f5f02bea59388b4facb677
unknown <mbfhk98@gmail.com>
2019-12-03 09:18:05 +0200

    input

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ console.log('the bot is online');
 })
 
 bot.on('message', msg=>{
+	var str = msg.content;
 	if(msg.content ==="HELLO"){
-		msg.reply('HELLO FRIEND');
+		msg.reply('HELLO FRIEND 2');
 	}
-	
+	var bjKey = "blackjack ";
+	var gambaI = str.indexOf(bjKey);
+	if(gambaI == 0){
+		var aLen = str.length - bjKey.length;
+		var amount = str.substr(bjKey.length,aLen);
+		msg.reply("Betting amount:" + amount);
+	}
+	if(msg.content.includes('blackjack')){
+		msg.reply('GAMBA!');
+	}
 })
 
 bot.login(token);
